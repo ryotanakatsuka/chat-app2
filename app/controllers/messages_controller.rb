497db2406a84fa2f1e9d4d7cd1438a97d96e3ac7
ryotanakatsuka <71533935+ryotanakatsuka@users.noreply.github.com>
2020-09-24 14:48:16 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,8 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
+
   end
   def create
     @room = Room.find(params[:room_id])
@@ -9,6 +11,8 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
+
       render :index
     end
   end
